95c7d55cd02a7813e15e13c34b4dca4b80b28be9
Satans-Underpants <128394890+Satans-Underpants@users.noreply.github.com>
2024-03-12 19:09:17 +0100

    Moved all the spells to the library

--- Mods/Sit_Happens/ScriptExtender/Lua/Server/Library.lua
+++ Mods/Sit_Happens/ScriptExtender/Lua/Server/Library.lua
@@ -16,6 +16,63 @@ ORIGINS = {
 
 
 FURNITURE = {
-    ["BaseChair"] = "e50cc36a-2eb4-4f73-a3bd-ba41accd5f7d",
-    ["TempleChair"] = "4c0a757a-bfba-470e-88f2-d6977f42870c", 
+    ["TempleChair"] = "4c0a757a-bfba-470e-88f2-d6977f42870c",
+    ["ElegantChairA"] = "e9c0102f-7eb9-4309-9f73-31d1d548d27b",
+    ["ElegantChairB"] = "561a8f5d-16bb-4ef6-b9d3-e42832c9bca8",
+    ["BoulderChair"] = "e1b6bb5a-c051-4333-b52f-075178a73389",
+    ["WoodChair"] = "a1dc6fa4-6572-4eed-85a0-4ea60d9a1104",
+    ["ElegantStool"] = "dda35d04-fc0e-41c5-aa9c-3c8b1ca1312a",
+    ["WoodStool"] = "4ceb4145-5f4b-42f1-890e-4c9bb0a1882d",
+    ["TavernStoolLarge"] = "8830c715-7e93-4910-b9a5-c042552e9769",
+    ["TavernStoolLargeOld"] = "c78c6292-b738-49ad-9cd5-0c12126dd9c5",
+    ["TavernStoolSmall"] = "0bf9b486-82df-48e5-9ce4-7cd89dd03a37",
+    ["TavernStoolSmallOld"] = "d6470e23-c5b4-44fa-926e-1572a5dc98e2",
+    ["StoolWoodPoor"] = "2544b579-1b07-4240-9d9e-7cf6e9a6f550",
+    ["PianoStoolGeneric"] = "cccf1f0a-616b-48b0-ad30-4bd478c730c8",
+    ["PianoStoolRich"] = "7b96ee7c-afa2-439f-925f-14b77a8428b8",
+    ["ShipSeats"] = "985274f6-561f-4c01-83b0-93acabc24eab",
+    ["DruidBenchA"] = "e4359d50-bebf-41c3-9f66-87301da59e37",
+    ["DruidBenchAMoss"] = "c517ba52-c39a-4f08-a9fd-4f75c71add72",
+    ["DungeonBenchA"] = "6aa95c72-9a0b-43fb-a634-810613bf576f",
+    ["DungeonBenchAMoss"] = "261fd3f6-2cb9-49b0-84a6-4c432537ca77",
+    ["DungeonBenchStoneAShar"] = "6164277a-7de8-4252-bdff-e267ae672535",
+    ["DungeonBenchStoneB"] = "4e6db2da-cfa4-4325-ade1-d86123f9a7a6",
+    ["DungeonBenchStoneBShar"] = "5edd735f-e2e4-4668-af4d-bf3e37899470",
+    ["DungeonBenchStoneC"] = "7a961a83-fb09-461e-a6e1-11fc3392a776",
+    ["DungeonBenchStoneCMoss"] = "daee76d1-b740-4203-b133-626c6cb33ad1",
+    ["DungeonBenchStoneCShar"] = "056945af-e9c2-4d8d-8497-752802e51ce1",
+    ["DungeonBenchStoneD"] = "63131eb6-6f86-43ba-89f8-a04637a42e1c",
+    ["DungeonBenchStoneE"] = "6e8af021-d819-4932-b9c9-e19b1910c6ed",
+    ["SharBenchBig"] = "136dddd2-b653-4c7d-ac7c-e8de21a0ff5b",
+    ["SharBenchBigGolden"] = "03e0d189-53bf-415d-9b83-1e766c2786a5",
+    ["SharBenchSmall"] = "55ed9b05-88a2-48b8-82b1-e8fca2613ce2",
+    ["SharBenchSmallGold"] = "7a64a666-7d59-4ee3-8fdf-7ad061edad2a",
+    ["BenchWoodPoor"] = "179cbd4f-71ec-4dbf-8746-be050b03408",
+    ["GlasshouseSofaA"] = "37751b04-585a-4242-aa6d-f0007bb492ce",
+    ["GlasshouseSofaB"] = "d46c8d9e-7c76-4e7d-9fb9-32418cc72cc7",
+    ["GlasshouseSofaC"] = "6b2d7bd5-5d2a-4bc7-a7ea-267cc60f84ac",
+    ["BenchRich"] = "cf317dd4-34f4-4035-a1ff-e5e6775c4368",
+    ["SofaBenchRichA"] = "415e850f-adc9-4b4e-83f6-3a4343ba35f1",
+    ["SofaBenchRichB"] = "71498970-96f9-4482-9bad-29b11c77a045",
+    ["TavernBenchWood"] = "0c45283d-7bd9-4e27-9c27-2028f6f71771",
+    ["TavernBenchWoodOld"] = "8610207c-1744-462d-96ee-a13bf1eae18",
+    ["ParliamentBenchDouble"] = "6ed52c4f-e62a-4164-a41e-b55719b51b44",
+    ["ParliamentBenchSingle"] = "43e71a31-189e-44e9-be1a-ee814f2ee4c7",
+    ["TempleBenchStone"] = "b7419c4f-e4c5-4721-b4b4-e28b8cec53cd",
+    ["ElegantBench"] = "0b94f7c0-2f73-4ea9-87b9-764cf51a5ca2",
+    ["TreeBench"] = "c3412269-0f2c-4675-8584-b1b6969e1918",
+    ["WoodenBench"] = "d90384de-6ab2-45a2-aec5-3a224bca33a5",
+    ["Sofa"] = "392017ce-4059-4afc-86f9-aeb1c7204445",
+    ["SofaRichA"] = "22e34173-7d83-445b-a871-033fb87b4d68",
+    ["SofaRichB"] = "68bdae4e-d333-4cb2-aefa-ea644c92f7a5",
+    ["TreeLog"] = "43e5e37e-b370-4ec6-8f2d-a178b97c4be9",
+    
+
+
+
+
+
+
+
+
 }
